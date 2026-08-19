85043755f3852fbf6d08a8a7f540b40eb0f85a73
Santiago Trini <santiagotrini@gmail.com>
2026-08-19 09:46:48 -0300

    agrega codigo para empezar

--- stringlength.s
+++ stringlength.s
@@ -1,1 +1,14 @@
 # escribir aca el ejercicio en MIPS
+.data
+palabra: .asciiz "hola"
+
+.text
+strlen:
+  # implementar strlen aca
+  jr      $ra
+main:
+  la      $a0, palabra
+  jal     strlen
+  # imprimir cantidad de letras
+  li      $v0, 10
+  syscall
