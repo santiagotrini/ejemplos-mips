ab09222d98db841b5852a7b6598f2aece9577c6b
Santiago Trini <santiagotrini@gmail.com>
2026-08-20 08:46:35 -0300

    solucion del ejercicio 4 en C, falta assembler

--- palindrome.c
+++ palindrome.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-int palindrome(char *s) {}
+int my_strlen(char *s) {
+  int i = 0;
+  while (s[i] != 0) i++;
+  return i;
+}
 
-int main(void) {}
+int palindrome(char *s) {
+  int length = my_strlen(s);
+  for (int i = 0; i < length/2; i++) 
+    if (s[i] != s[length-i-1]) return 0;
+  return 1;
+}
+
+int main(void) {
+  char *s = "radar";
+  char *t = "abba";
+  char *u = "valentina";
+  printf("%s %s palindromo\n", s, palindrome(s) ? "es" : "no es");
+  printf("%s %s palindromo\n", t, palindrome(t) ? "es" : "no es");
+  printf("%s %s palindromo\n", u, palindrome(u) ? "es" : "no es");
+  return 0;
+}
